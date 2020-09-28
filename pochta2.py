72c022d5fe4f4112c499bc618231ceb87283f111
exel14 <showmetheway220@gmail.com>
2020-09-28 18:05:42 +0600

    Searching system for shop

--- pochta2.py
+++ pochta2.py
@@ -1,32 +1,43 @@
 import sendgrid
 from sendgrid import Mail
+
 sendgrid_api_key = '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'
 SUBJECT = 'Code'
 sg = sendgrid.SendGridAPIClient(sendgrid_api_key)
+
+
 def sendgrid_email(code, email):
-    body = 'Hi,'+ code
+    body = 'Hi,' + code
     message = Mail(
         from_email='showmetheway220@gmail.com',
         to_emails=email,
         subject=SUBJECT,
         plain_text_content=body)
     response = sg.send(message)
-def registration(username,password,check_password):
+
+
+def registration(username, password, check_password):
     global code
     code = '123456'
     if password == check_password:
-        sendgrid_email(code,'showmetheway220@gmail.com')
+        sendgrid_email(code, 'showmetheway220@gmail.com')
     else:
         return 'Повторите попытку'
+
+
 username = 'username'
 password = 'password'
 check_password = 'password'
-registration(username=username,password=password,check_password=check_password)
+registration(username=username, password=password, check_password=check_password)
+
+
 def check_code(check):
     if check == code:
         print(code)
     else:
         print('NO')
+
+
 check = '123456'
 check_code(check=check)
 strings = ['Samsung j5',
@@ -49,17 +60,23 @@ strings = ['Samsung j5',
            'SSD Adata 1TB',
            'Acer zenbook',
            'Asus razer']
+
+
 def write_to(strings):
-       with open('message2.txt', 'w') as file1:
-            for sentence in strings:
-                file1.write(sentence.lower() + '\n')
+    with open('message2.txt', 'w') as file1:
+        for sentence in strings:
+            file1.write(sentence.lower() + '\n')
+
+
 write_to(strings)
+
+
 def red_lines():
     with open('message2.txt', 'r') as file2:
         var = file2.readlines()
         for i in var:
             if i.startswith('iphone') or i.startswith('samsung'):
-                with open('telephone.txt','a')as file3:
+                with open('telephone.txt', 'a')as file3:
                     file3.write(i)
             elif i.startswith('asus') or i.startswith('acer') or i.startswith('mac') or i.startswith('macbook'):
                 with open('computers.txt', 'a')as file4:
@@ -68,13 +85,19 @@ def red_lines():
                 with open('hard_disk.txt', 'a') as file5:
                     file5.write(i)
             elif i.startswith('geforce') or i.startswith('amd'):
-                with open('processor.txt','a') as file6:
+                with open('processor.txt', 'a') as file6:
                     file6.write(i)
+
+
 red_lines()
+
+
 def open_files(file_name):
-    with open(file_name,'r') as read1:
+    with open(file_name, 'r') as read1:
         phones = read1.read()
     return phones
+
+
 user_input = input()
 if user_input == 'phones':
     print(open_files('telephone.txt'))
@@ -84,18 +107,3 @@ elif user_input == 'hard_disk':
     print(open_files('hard_disk.txt'))
 elif user_input == 'computers':
     print(open_files('computers.txt'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
